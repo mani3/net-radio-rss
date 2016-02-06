19a83704d1fa689583d43caeb2dc6fec1c2e5fb4
Kazuya Shida <kazuya.4da@gmail.com>
2016-02-07 05:24:46 +0900

    Minor fixed

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -3,6 +3,6 @@ import multiprocessing
 bind = '127.0.0.1:4002'
 workers = multiprocessing.cpu_count() * 2 + 1
 timeout = 30
-accesslog = '/var/log/internet_radio/access.log'
-errorlog = '/var/log/internet_radio/error.log'
-proc_name = 'gunicorn_internet_radio'
+accesslog = '/var/log/net_radio_rss/access.log'
+errorlog = '/var/log/net_radio_rss/error.log'
+proc_name = 'gunicorn_net_radio_rss'
