91c8057e552849ff9a33e59c8fc0448b1b9ec1b8
Kazuya Shida <kazuya.4da@gmail.com>
2016-02-09 00:37:07 +0900

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+ENV_DIR=flask
+if [ ! -d "${ENV_DIR}" ]; then
+  ./env.sh ${ENV_DIR}
+fi
+
+source ${ENV_DIR}/bin/activate
+
+# Run migration
+if [ ! -d "migrations" ]; then
+  python manage.py db init
+fi
+python manage.py db migrate -m "`date '+%Y%m%d%H%M'`"
+python manage.py db show
+python manage.py db upgrade
+
+gunicorn -c gunicorn_config.py
+deactivate
+
